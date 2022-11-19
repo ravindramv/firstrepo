090b44b8b51d22a22a7cf4e206381c258fbd4041
ravindramv <ravindramv@gmail.com>
2022-11-19 14:24:51 +0000

    commiting

--- scripgit.sh
+++ scripgit.sh
@@ -3,4 +3,5 @@ git clone "https://github.com/ravindramv/firstrepo.git"
 echo "enter the file name"
 read string
 git add .;git commit -m "commiting"
-git push origin 
+git push origin master
+echo "push the repo"
